1cd8ec9532e5519a3c11c2214e167a07e992027c
Axionauta <daniln2209@gmail.com>
2022-07-13 13:48:26 -0300

    adição de scripts no botão a esquerda

--- Impulse Javascript/To do list/assets/js/script.js
+++ Impulse Javascript/To do list/assets/js/script.js
@@ -11,15 +11,20 @@ livro.atual = livro.paginas.length-1;
 bright.onclick = () => {
     livro.paginas[livro.atual].style.transform = "rotateX(30deg) rotateY(-180deg)";
     let zindex = window.getComputedStyle(livro.paginas[livro.atual]).getPropertyValue("z-index");
-    livro.atual--;
-    setTimeout(function(){
-        livro.paginas[livro.atual].style.zIndex = parseInt(zindex)+2;
-        alert(parseInt(zindex));
-    }, 1000);
+    if (livro.atual > 0) {
+        livro.atual--;
+        setTimeout(function(){
+            livro.paginas[livro.atual].style.zIndex = parseInt(zindex)+2;
+        }, 1000);
+    }
 };
 
 bleft.onclick = () => {
-    livro.paginas[livro.atual].style.transform = "rotateX(30deg) rotateY(180deg)";
-    livro.atual++;
+    if (livro.atual <= (livro.paginas.length-1)) {
+        let zindex= window.getComputedStyle(livro.paginas[livro.atual]).getPropertyValue("z-index");
+        livro.paginas[livro.atual+1].style.transform = "rotateX(30deg) rotateY(0deg)";
+        livro.paginas[livro.atual+1].style.zIndex=parseInt(zindex)+2;
+        livro.atual++;
+    }
 };
 
