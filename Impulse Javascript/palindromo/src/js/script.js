43b97d5d1840a3888c4c1407f1e6ed49482dece7
Axionauta <daniln2209@gmail.com>
2022-07-29 18:19:46 -0300

    efeito hover nos links e no botão; impedimento de envio do formulário

--- Impulse Javascript/palindromo/src/js/script.js
+++ Impulse Javascript/palindromo/src/js/script.js
@@ -14,17 +14,18 @@ submit.onclick = () => {
     }
     valor_textarea = valor_textarea.toLowerCase();
     let valor_nospace = valor_textarea.replace(/\s/g, "");
-    valor_nospace = valor_nospace.replace ( /[^a-zA-Z0-9]/g, "");
     letras_acentos.acentos.forEach ((acentuadas, index) => {
         acentuadas.forEach (acento => {
             let regex = new RegExp (acento, "g");
             valor_nospace = valor_nospace.replace (regex, letras_acentos.letras[index]);
         })
     });
+    valor_nospace = valor_nospace.replace ( /[^a-zA-Z0-9]/g, "");
     const valor_contrario = valor_nospace.split("").reverse().join().replace(/,/g, "");
     if (valor_nospace == valor_contrario) {
         resultado.innerHTML = "Resultado: <strong class='sim-palindromo'> É um palíndromo.</strong>";
     } else {
         resultado.innerHTML = "Resultado: <strong class='nao-palindromo'> Não é um palíndromo.</strong>";
     }
+    return false;
 };
