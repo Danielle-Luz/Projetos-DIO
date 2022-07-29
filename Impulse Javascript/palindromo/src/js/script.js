8479832ca4d0e54f9a446574a3039f22da4d834c
Axionauta <daniln2209@gmail.com>
2022-07-29 17:54:37 -0300

    remoção de caracteres especiais e substituição de acentuação

--- Impulse Javascript/palindromo/src/js/script.js
+++ Impulse Javascript/palindromo/src/js/script.js
@@ -1,15 +1,30 @@
 const textarea = document.getElementsByName ("palindromo")[0];
 const submit = document.querySelector ("button");
 const resultado = document.getElementById ("resultado");
+const letras_acentos = {
+    letras: ["a", "e", "i", "o", "u"],
+    acentos: [["à", "á", "ã", "â"], ["è", "é", "ê"], ["ì", "í", "î"], ["ò", "ó", "õ", "ô"], ["ù", "ú", "û"]]
+};
 
 submit.onclick = () => {
-    const valor_textarea = textarea.value.toLowerCase();
-    const valor_nospace = valor_textarea.replace(/\s/g, "");
+    let valor_textarea = textarea.value
+    if (!valor_textarea) {
+        alert ("Insira um valor no campo.");
+        return;
+    }
+    valor_textarea = valor_textarea.toLowerCase();
+    let valor_nospace = valor_textarea.replace(/\s/g, "");
+    valor_nospace = valor_nospace.replace ( /[^a-zA-Z0-9]/g, "");
+    letras_acentos.acentos.forEach ((acentuadas, index) => {
+        acentuadas.forEach (acento => {
+            let regex = new RegExp (acento, "g");
+            valor_nospace = valor_nospace.replace (regex, letras_acentos.letras[index]);
+        })
+    });
     const valor_contrario = valor_nospace.split("").reverse().join().replace(/,/g, "");
     if (valor_nospace == valor_contrario) {
         resultado.innerHTML = "Resultado: <strong class='sim-palindromo'> É um palíndromo.</strong>";
     } else {
         resultado.innerHTML = "Resultado: <strong class='nao-palindromo'> Não é um palíndromo.</strong>";
     }
-    return false;
 };
